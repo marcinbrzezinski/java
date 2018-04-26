cb2ce66c4472aa73158234e307d3b3bf39d8cf4a
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-04-26 17:41:24 +0200

    Initial commit.

---
+++ Hello/Hello.java
@@ -0,0 +1,7 @@
+class Hello {
+	public static void main(String[] arguments){
+	// Wykonanie programu zaczyna się tutaj
+		System.out.println("Hello world.");
+		System.out.println("Linia numer dwa :)");
+	}
+}
