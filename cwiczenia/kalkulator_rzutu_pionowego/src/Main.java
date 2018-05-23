d54c67168c424c1095c68b601c93211c0b69a947
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-05-23 16:10:11 +0200

    Kalkulator rzutu pionowego

---
+++ cwiczenia/kalkulator_rzutu_pionowego/src/Main.java
@@ -0,0 +1,16 @@
+// Kalkulator rzutu pionowego
+// x(t) = 0.5 * at^2 + vjt + xj
+
+class GravityCalculator {
+    public static void main(String[] args){
+        double gravity = -9.81;
+        double initialVelocity = 0.0;
+        double fallingTime = 10.0;
+        double initialPosition = 0.0;
+        double finalPosition = .5 * gravity * fallingTime * fallingTime;
+        finalPosition = finalPosition + initialVelocity * fallingTime;
+        finalPosition = finalPosition + initialPosition;
+
+        System.out.println("Pozycja obiektu po " + fallingTime + " sekundach wynosi " + finalPosition + "m.");
+    }
+}
