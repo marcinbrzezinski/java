35852fd0ebd318a484e6b0293f6e3d45fddf34a8
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-05-23 16:22:19 +0200

    Zakres

---
+++ cwiczenia/zakres/src/Main.java
@@ -0,0 +1,15 @@
+package Zakres;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int x = 5;
+    if (x==5){
+        x = 6;
+        int y = 72;
+        System.out.println("x = " + x + ", y =" + y);
+    }
+
+        System.out.println("x = " + x + ", y = ?"  );
+    }
+}
