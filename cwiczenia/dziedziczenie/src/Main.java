03958fc3b635611286cb1fbc5da8b4024a112cac
Marcin Brzeziński <32923504+marcinbrzezinski@users.noreply.github.com>
2018-06-07 10:18:51 +0200

    Dziedziczenie

--- cwiczenia/dziedziczenie/src/Main.java
+++ cwiczenia/dziedziczenie/src/Main.java
@@ -6,21 +6,22 @@ public class Main {
 //       jesiotr.poruszajSie();
 //       System.out.println(jesiotr);
 
-       Zwierze[] zwierzeta = new Zwierze[3];        // <== lepsza wersja tego co wyzej
+        Zwierze[] zwierzeta = new Zwierze[3];        // <== lepsza wersja tego co wyzej
 // polimorfizm
-       zwierzeta[0] = new Jamnik();
-       zwierzeta[1] = new Jesiotr();
-       zwierzeta[2] = new Szpak();
+        zwierzeta[0] = new Jamnik();
+        zwierzeta[1] = new Jesiotr();
+        zwierzeta[2] = new Szpak();
 
 //       for (int indeks = 0; indeks < 3; indeks++){
 //           zwierzeta[indeks].poruszajSie();        //   <= petla bedzie dzialala tak samo jak ta nizej
 //           System.out.println(zwierzeta[indeks]);
-        for (Zwierze zwierze : zwierzeta){
+        for (Zwierze zwierze : zwierzeta) {
             System.out.println(zwierze);
+
             zwierze.poruszajSie();
 
 
-
-       }
+        }
     }
 }
+
